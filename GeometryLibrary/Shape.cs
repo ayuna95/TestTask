c6e5de38ef09235edcce322810a2bc700ada9510
ayuna95 <44255712+ayuna95@users.noreply.github.com>
2023-09-14 16:22:12 +0800

    GeometryLibrary
    GeometryLibrary

---
+++ GeometryLibrary/Shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace GeometryLibrary
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        // Метод для вычисления площади круга по радиусу
+        public double CalculateArea()
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус круга должен быть положительным числом.");
+            }
+
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+        // Метод для вычисления площади треугольника по трём сторонам
+        public double CalculateArea()
+        {
+            if (!IsTriangleValid(sideA, sideB, sideC))
+            {
+                throw new ArgumentException("Треугольник с такими сторонами не существует.");
+            }
+
+            double s = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+        }
+        //Метод для проверки на то, является ли треугольник прямоугольным
+        public bool IsRightTriangle()
+        {
+            // Сортируем стороны по возрастани
+            double[] sides = { sideA, sideB, sideC };
+            Array.Sort(sides);
+            // Проверяем теорему Пифагора
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+        // Метод для проверки правильности треугольника по трём сторонам
+        private bool IsTriangleValid(double sideA, double sideB, double sideC)
+        {
+            return sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA;
+        }
+    }
+}
---
+++ GeometryLibrary/UnitTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using GeometryLibrary;
+using System;
+
+namespace GeometryLibrary.Tests
+{
+    [TestFixture]
+    public class GeometryCalculatorTests
+    {
+        [Test]
+        public void CalculateCircleArea_ValidRadius_ReturnsCorrectArea()
+        {
+            // Arrange
+            double radius = 5.0;
+            Circle circle = new Circle(radius);
+
+            // Act
+            double area = circle.CalculateArea();
+
+            // Assert
+           Assert.AreEqual(Math.PI * Math.Pow(radius, 2), area, 0.001);
+        }
+
+        [Test]
+        public void CalculateTriangleArea_ValidSides_ReturnsCorrectArea()
+        {
+            // Arrange
+            double sideA = 3.0;
+            double sideB = 4.0;
+            double sideC = 5.0;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            // Act
+            double area = triangle.CalculateArea();
+
+            // Assert
+            Assert.AreEqual(6.0, area, 0.001);
+        }
+
+        [Test]
+        public void IsRightTriangle_ValidRightTriangle_ReturnsTrue()
+        {
+            // Arrange
+            double sideA = 3.0;
+            double sideB = 4.0;
+            double sideC = 5.0;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            // Act
+            bool isRightTriangle = triangle.IsRightTriangle();
+
+            // Assert
+            Assert.IsTrue(isRightTriangle);
+        }
+    }
+}
